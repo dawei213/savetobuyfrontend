ca69bb5aed1a00f1cf2dc9ac88fed00d80783b4e
David Wei <dawei213@gmail.com>
2018-03-26 16:22:57 -0400

    Added cost and period.

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,8 @@ const functions = require('firebase-functions');
 const NAME_ACTION = 'make_name';
 const COLOR_ARGUMENT = 'color';
 const NUMBER_ARGUMENT = 'number';
+const COST_ARGUMENT = 'unit-currency';
+const DURATION_ARGUMENT = 'duration';
 
 exports.savetobuyfrontend = functions.https.onRequest((request, response) => {
   const app = new DialogflowApp({request, response});
@@ -17,9 +19,15 @@ exports.savetobuyfrontend = functions.https.onRequest((request, response) => {
   function makeName (app) {
     let number = app.getArgument(NUMBER_ARGUMENT);
     let color = app.getArgument(COLOR_ARGUMENT);
+    let cost = app.getArgument(COST_ARGUMENT);
+    let duration = app.getArgument(DURATION_ARGUMENT);
+    console.log('-----COST-----:' + JSON.stringify(cost));
+    console.log('-----DURATION------:' + JSON.stringify(duration));
     app.tell('Alright, your silly name is ' +
       color + ' ' + number +
-      '! I hope you like it. See you next time.');
+      '! It will cost you ' + cost.amount + ' ' + cost.currency + ' over a period of ' + duration.amount + ' ' + duration.unit + '. That is all. No soup for you! Have a good day.');
+   
+    app.tell('Ok, we will transfer <X> amount on a weekly basis until you have reached your goal. Thank you for using Save to buy.');
   }
 
   let actionMap = new Map();
